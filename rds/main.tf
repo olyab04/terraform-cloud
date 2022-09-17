535ba8a35d045b412fb99bfe92c32d981e2cf195
olyab04 <107557891+olyab04@users.noreply.github.com>
2022-09-17 15:57:20 -0500

    added

--- rds/main.tf
+++ rds/main.tf
@@ -116,29 +116,3 @@ output "subnet1" {
 		aws_default_subnet.default_az3.id,
 	]
 }
-
-
-resource "aws_db_subnet_group" "db" {
-	name = "db"
-	subnet_ids = [
-		aws_default_subnet.default_az1.id,
-		aws_default_subnet.default_az2.id,
-		aws_default_subnet.default_az3.id,
-	]
-}
-
-
-resource "aws_db_instance" "default" {
-	identifier = "dbname"
-	allocated_storage = 20
-	storage_type = "gp2"
-	engine = "mysql"
-	engine_version = "5.7"
-	instance_class = "db.t2.micro"
-	db_name = "mydb"
-	username = "admin"
-	password = random_password.random.result
-	publicly_accessible = true
-	db_subnet_group_name = aws_db_subnet_group.db.name
-	skip_final_snapshot = true #used to delete the repo in the future without this you cant delete. There are bugs reported 
-}
